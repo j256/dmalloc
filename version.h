fa53f4ec7ae6ac0cc76ea000517a00f0691b4062
gray <gray>
1997-01-16 20:47:17 +0000

    updated the version ID

--- version.h
+++ version.h
@@ -21,7 +21,7 @@
  *
  * The author may be contacted at gray.watson@letters.com
  *
- * $Id: version.h,v 1.62 1995/12/29 01:58:48 gray Exp $
+ * $Id: version.h,v 1.63 1997/01/16 20:47:17 gray Exp $
  */
 
 #ifndef __VERSION_H__
@@ -37,8 +37,8 @@
  *	News
  *	dmalloc.texi (2 places)
  */
-LOCAL	char	*dmalloc_version = "3.1.3";
+LOCAL	char	*dmalloc_version = "3.2.0";
 
-/* Version Date: $Date: 1995/12/29 01:58:48 $ */
+/* Version Date: $Date: 1997/01/16 20:47:17 $ */
 
 #endif /* ! __VERSION_H__ */
