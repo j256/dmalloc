7edb0274fd7ba2e1c2c81dc364bfd6f4ad713b67
gray <gray>
1998-11-16 16:56:05 +0000

    3.1.0 release

--- version.h
+++ version.h
@@ -21,7 +21,7 @@
  *
  * The author may be contacted via http://www.letters.com/~gray/
  *
- * $Id: version.h,v 1.76 1998/11/09 19:36:28 gray Exp $
+ * $Id: version.h,v 1.77 1998/11/16 16:56:05 gray Exp $
  */
 
 #ifndef __VERSION_H__
@@ -35,8 +35,8 @@
  *	NEWS
  *	dmalloc.texi (2 places)
  */
-static	char	*dmalloc_version = "4.1.0-beta";
+static	char	*dmalloc_version = "4.1.0";
 
-/* Version Date: $Date: 1998/11/09 19:36:28 $ */
+/* Version Date: $Date: 1998/11/16 16:56:05 $ */
 
 #endif /* ! __VERSION_H__ */
