c4a2716f2096bc8a768d3f81247514a1248f5e2e
gray <gray>
2003-05-15 02:47:28 +0000

    removed debug_val.h

--- debug_tok.h
+++ debug_tok.h
@@ -18,26 +18,88 @@
  *
  * The author may be contacted via http://dmalloc.com/
  *
- * $Id: debug_tok.h,v 1.35 2003/05/13 16:37:44 gray Exp $
+ * $Id: debug_tok.h,v 1.36 2003/05/15 02:47:28 gray Exp $
  */
 
 #ifndef __DEBUG_TOK_H__
 #define __DEBUG_TOK_H__
 
+#include "dmalloc_loc.h"		/* for BIT_FLAG */
+
 /*
- * NOTE: see debug_val.h for instructions about changes here
+ * special debug codes which detail what debug features are enabled
+ * NOTE: need to change debug_tok.h, mallocrc, and malloc.texi if any
+ * capabilities are added/removed/updated
  */
 
-#include "dmalloc_loc.h"		/* for LOCAL */
-#include "debug_val.h"			/* for debug codes */
+/* logging */
+#define DEBUG_LOG_STATS		BIT_FLAG(0)	/* generally log statistics */
+#define DEBUG_LOG_NONFREE	BIT_FLAG(1)	/* report non-freed pointers */
+#define DEBUG_LOG_KNOWN		BIT_FLAG(2)	/* report only known nonfreed*/
+#define DEBUG_LOG_TRANS		BIT_FLAG(3)	/* log memory transactions */
+/* 4 available - 20001107 */
+#define DEBUG_LOG_ADMIN		BIT_FLAG(5)	/* log background admin info */
+/* 6 available 20030508 */
+/* 7 available - 20001107 */
+#define DEBUG_LOG_BAD_SPACE	BIT_FLAG(8)	/* dump space from bad pnt */
+#define DEBUG_LOG_NONFREE_SPACE	BIT_FLAG(9)	/* dump space from non-freed */
 
+#define DEBUG_LOG_ELAPSED_TIME	BIT_FLAG(18)	/* log pnt elapsed time info */
+#define DEBUG_LOG_CURRENT_TIME	BIT_FLAG(19)	/* log pnt current time info */
+
+/* checking */
+#define DEBUG_CHECK_FENCE	BIT_FLAG(10)	/* check fence-post errors  */
+#define DEBUG_CHECK_HEAP	BIT_FLAG(11)	/* examine heap adm structs */
+#define DEBUG_CHECK_LISTS	BIT_FLAG(12)	/* check the free lists */
+#define DEBUG_CHECK_BLANK	BIT_FLAG(13)	/* check blank sections */
+#define DEBUG_CHECK_FUNCS	BIT_FLAG(14)	/* check functions */
+/* 15 available - 20001107 */
+
+/* misc */
+#define DEBUG_FORCE_LINEAR	BIT_FLAG(16)	/* force linear heap */
+#define DEBUG_CATCH_SIGNALS	BIT_FLAG(17)	/* catch HUP, INT, and TERM */
+/* 18,19 used above */
+#define DEBUG_REALLOC_COPY	BIT_FLAG(20)	/* copy all reallocations */
+#define DEBUG_FREE_BLANK	BIT_FLAG(21)	/* write over free'd memory */
+#define DEBUG_ERROR_ABORT	BIT_FLAG(22)	/* abort on error else exit */
+#define DEBUG_ALLOC_BLANK	BIT_FLAG(23)	/* write over to-be-alloced */
+/* 24 available - 20030508 */
+#define DEBUG_PRINT_MESSAGES	BIT_FLAG(25)	/* write messages to STDERR */
+#define DEBUG_CATCH_NULL	BIT_FLAG(26)	/* quit before return null */
+#define DEBUG_NEVER_REUSE	BIT_FLAG(27)	/* never reuse memory */
+#define DEBUG_ERROR_FREE_NULL	BIT_FLAG(28)	/* catch free(0) */
+/* 29 available - 20011130 */
+#define DEBUG_ERROR_DUMP	BIT_FLAG(30)	/* dump core on error */
+/* 31 is the high bit and off-limits */
+
+/*
+ * Flags that after being set or not/set at process start-up, cannot
+ * be removed.
+ *
+ * NOTE: CHECK_FENCE _can_ be removed since the pnt_below/above_adm
+ * values are never reset in chunk.
+ */
+#define DEBUG_NOT_REMOVABLE	(DEBUG_CATCH_SIGNALS)
+
+/*
+ * flags that cannot be added after process start-up.
+ */
+#define DEBUG_NOT_ADDABLE	(DEBUG_CHECK_FENCE | DEBUG_CHECK_BLANK)
+
+/*
+ * structure for mapping the string to the numerical token
+ */
 typedef struct {
   char		*at_string;		/* attribute string */
   unsigned long	at_value;		/* value for the item */
   char		*at_desc;		/* description string */
 } attr_t;
 
-static	attr_t		attributes[] = {
+static	attr_t		attributes[]
+#ifdef __GNUC__
+__attribute__ ((unused))
+#endif
+= {
   { "none",		0,
       "no functionality" },
   
