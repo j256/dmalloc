10d7f01a428823ded68c8e399370b210c39ea79a
Gray Watson <gray.github@mailnull.com>
2020-11-22 17:59:44 -0500

    final tweaks

--- release.sh
+++ release.sh
@@ -6,34 +6,30 @@
 set -e
 
 cwd=`pwd`
+version=`grep dmalloc_version version.h | cut -f2 -d\"`
+dir=dmalloc-$version
 
 # run tests
 echo "Running tests"
-rm -rf dmalloc
+rm -rf $dir
 # clone our repo
-git clone git@github.com:j256/dmalloc.git
-cd dmalloc
-mkdir tmp
-cd tmp
+git clone git@github.com:j256/dmalloc.git $dir
+mkdir $dir/tmp
+cd $dir/tmp
 ../configure
 make all heavy
 dmalloc
 
 echo "----------------------------------------------------------------"
 echo "Building release"
-
 cd $cwd
-rm -rf dmalloc
-# clone our repo
-git clone git@github.com:j256/dmalloc.git
-# clear our dot files not suitable for release
-rm -rf dmalloc/.git dmalloc/.gitignore dmalloc/.circleci
 
-version=`grep dmalloc_version version.h | cut -f2 -d\"`
-dir=dmalloc-$version
 echo "Building release directory: $dir"
 rm -rf $dir
-mv dmalloc $dir
+# clone our repo
+git clone git@github.com:j256/dmalloc.git $dir
+# clear our dot files not suitable for release
+rm -rf $dir/.git $dir/.gitignore $dir/.circleci
 
 dest=$dir.tar.gz
 rm -f $dest
